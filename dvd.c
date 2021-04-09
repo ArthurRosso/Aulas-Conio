562344904fbf3cd050020e82f753580061588e41
ArthurRosso <arthurderosso@gmail.com>
2021-04-09 14:41:11 -0300

    commit 4 aula 2

--- dvd.c
+++ dvd.c
@@ -4,6 +4,9 @@
 #include <time.h>
 #include <conio2.h>
 
+#define LARGURA_TELA 80
+#define ALTURA_TELA 30
+
 typedef struct {
     int x;
     int y;
@@ -12,6 +15,8 @@ typedef struct {
 typedef struct {
     Posicao pos;
     float timer;
+    int velX;
+    int velY;
 } DVD;
 
 typedef struct {
@@ -44,25 +49,49 @@ float CalculaDeltaT() {
     return delta;
 }
 
-Jogo InicializaJogo(){
+Jogo InicializaJogo() {
     Jogo j;
     j.dvd.pos.x=0;
     j.dvd.pos.y=0;
+    j.dvd.velX=1;
+    j.dvd.velY=1;
     j.dvd.timer=0;
 
     return j;
 }
 
-void AtualizaJogo(Jogo* j, float deltaT){
+void AtualizaJogo(Jogo* j, float deltaT) {
     j->dvd.timer+=deltaT;
 
-    if(j->dvd.timer>=1){
-        j->dvd.pos.x++;
-        j->dvd.pos.y++;
-        j->dvd.timer-=1;
+    if(j->dvd.timer>=0.1) {
+        j->dvd.pos.x += j->dvd.velX;
+        j->dvd.pos.y += j->dvd.velY;
+        j->dvd.timer-=0.1;
+
+
+        if (j->dvd.pos.x >= LARGURA_TELA) {
+            j->dvd.velX *= -1;
+            j->dvd.pos.x = LARGURA_TELA-3;
+
+        }
+
+        if (j->dvd.pos.y > ALTURA_TELA) {
+            j->dvd.velY *= -1;
+            j->dvd.pos.y = ALTURA_TELA-1;
+        }
+
+        if (j->dvd.pos.x <= 0) {
+            j->dvd.velX *= -1;
+            j->dvd.pos.x = 0;
+        }
+
+        if (j->dvd.pos.y <= 0) {
+            j->dvd.velY *= -1;
+            j->dvd.pos.y = 0;
+        }
     }
 }
-void DesenhaJogo(Jogo* j){
+void DesenhaJogo(Jogo* j) {
     clrscr();
 
     cputsxy(
